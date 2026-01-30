89945771e964915a750825ceb0962b3954e4c4f8
inamprograms <inamul09rehman@gmail.com>
2026-01-30 16:08:33 +0500

    React list rendering using map

--- beginning_react/my-react-app/src/App.jsx
+++ beginning_react/my-react-app/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const users = [
+    { id: 1, name: 'Nathan', role: 'Web Developer' },
+    { id: 2, name: 'John', role: 'Web Designer' },
+    { id: 3, name: 'Jane', role: 'Team Leader' },
+  ]
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>The currently active users list:</p>
+      <ul>
+      {
+        users.map(function(user, index){
+          // returns Nathan, then John, then Jane
+          return (
+            <li key={index}> {user.name} as the {user.role} </li>
+          )
+        })
+      }
+      </ul>
     </>
   )
 }
-
 export default App
